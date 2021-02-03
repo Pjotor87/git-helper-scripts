3f38c37d17d27a06f2461ca15816d71661ba212f
Peter <peter.a@codium.co>
2021-02-03 11:37:55 +0700

    Update script

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 ### cd into the folder where development is done and execute this script to install
 
 git clone https://github.com/Pjotor87/git-helper-scripts.git
+
 # Remove the git directory at the root
 rm -rf git-helper-scripts/.git
 # And the Readme file
 rm git-helper-scripts/README.md
+# And the update file
+rm git-helper-scripts/update.sh
 # And this install script file
 rm git-helper-scripts/install.sh
+
 # Move the clone script to the root directory
 mv git-helper-scripts/clone-new-project.sh clone-new-project.sh
 # Move the helper scripts directory one level up
---
+++ update.sh
@@ -0,0 +1,6 @@
+### cd into the folder where development is done and execute this script to update
+
+rm -rf git-helper-scripts
+rm clone-new-project.sh
+
+sh <(curl -Ls https://raw.githubusercontent.com/Pjotor87/git-helper-scripts/master/install.sh)
